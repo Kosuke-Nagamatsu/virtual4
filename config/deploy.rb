719466e135368a85f0cb2a05a80095a5922280e3
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-07-29 00:13:30 +0900

    修正3：deploy checkのため

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.6.0'
 set :application, 'virtual4'
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'https://github.com/Kosuke-Nagamatsu/test'
+set :repo_url, 'https://github.com/Kosuke-Nagamatsu/virtual4'
 # deployするブランチ。デフォルトでmainを使用している場合、masterをmainに変更してください。
 set :branch, ENV['BRANCH'] || 'master'
 # deploy先のディレクトリ。
